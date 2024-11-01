06962093de5bc146704def638d83ad7ef9a227da
Miguel Pereira <miguel.b.per@gmail.com>
2024-11-01 15:26:02 +0000

    Allow arbitrary types in pydantic model

--- src/utils/types.py
+++ src/utils/types.py
@@ -1,12 +1,14 @@
 from lightning import LightningDataModule, LightningModule, Trainer
 from omegaconf import DictConfig
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from torch import Tensor
 
 Metrics = dict[str, Tensor]
 
 
 class Objects(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     cfg: DictConfig
     model: LightningModule
     datamodule: LightningDataModule
