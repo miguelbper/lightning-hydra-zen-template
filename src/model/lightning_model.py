f8abcd39a123a5e2daf60b24240e8affffbbf922
Miguel Pereira <miguel.b.per@gmail.com>
2024-11-05 19:07:18 +0000

    Implement forward with task outputs

--- src/model/lightning_model.py
+++ src/model/lightning_model.py
@@ -6,7 +6,14 @@ from torch.nn.modules.loss import _Loss
 from torch.optim.optimizer import Optimizer
 from torchmetrics import MetricCollection
 
-from src.utils.types import Batch
+from src.utils.types import (
+    Batch,
+    BinaryClassificationOutput,
+    MulticlassClassificationOutput,
+    RegressionOutput,
+    Split,
+    Task,
+)
 
 
 class LightningModel(LightningModule):
@@ -16,39 +23,47 @@ class LightningModel(LightningModule):
         loss_fn: _Loss,
         optimizer: Optimizer,
         metric_collection: MetricCollection,
+        task: Task,
     ) -> None:
         super().__init__()
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.metric_collection = metric_collection
+        self.task = task
+
         self.metrics = {
-            "train": self.metric_collection.clone(prefix="train/"),
-            "val": self.metric_collection.clone(prefix="val/"),
-            "test": self.metric_collection.clone(prefix="test/"),
+            Split.TRAIN: self.metric_collection.clone(prefix=f"{Split.TRAIN.value}/"),
+            Split.VAL: self.metric_collection.clone(prefix=f"{Split.VAL.value}/"),
+            Split.TEST: self.metric_collection.clone(prefix=f"{Split.TEST.value}/"),
         }
+        self.output_cls_name = {
+            Task.REGRESSION: RegressionOutput,
+            Task.BINARY_CLASSIFICATION: BinaryClassificationOutput,
+            Task.MULTICLASS_CLASSIFICATION: MulticlassClassificationOutput,
+        }[task]
 
-    def forward(self, *args: Any, **kwargs: Any) -> Any:
-        # TODO: implement forward pass
-        pass
+    def forward(self, inputs: Tensor) -> Any:
+        logits = self.model(inputs)
+        return self.output_cls_name(logits=logits)
 
-    def step(self, batch: Batch, batch_idx: int, split: str) -> Tensor:
+    def step(self, batch: Batch, batch_idx: int, split: Split) -> Tensor:
         inputs, target = batch
         logits = self.model(inputs)
         loss = self.loss_fn(logits, target)
-        self.log(f"{split}/loss", loss, on_step=True, on_epoch=False)
+        self.log(f"{split.value}/loss", loss, on_step=True, on_epoch=False)
         self.metrics[split](logits, target)
         self.log_dict(self.metrics[split], on_step=True, on_epoch=True)
         return loss
 
     def training_step(self, batch: Batch, batch_idx: int) -> Tensor:
-        return self.step(batch, batch_idx, "train")
+        return self.step(batch, batch_idx, Split.TRAIN)
 
     def validation_step(self, batch: Batch, batch_idx: int) -> None:
-        self.step(batch, batch_idx, "val")
+        self.step(batch, batch_idx, Split.VAL)
 
     def test_step(self, batch: Batch, batch_idx: int) -> None:
-        self.step(batch, batch_idx, "test")
+        self.step(batch, batch_idx, Split.TEST)
 
     def configure_optimizers(self) -> Optimizer:
         # TODO: include scheduler
--- src/utils/types.py
+++ src/utils/types.py
@@ -1,7 +1,9 @@
+from enum import Enum, auto
+
 from lightning import LightningDataModule, LightningModule, Trainer
 from omegaconf import DictConfig
-from pydantic import BaseModel, ConfigDict
-from torch import Tensor
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+from torch import Tensor, sigmoid, softmax
 
 Batch = tuple[Tensor, Tensor]
 Metrics = dict[str, Tensor]
@@ -14,3 +16,57 @@ class Objects(BaseModel):
     model: LightningModule
     datamodule: LightningDataModule
     trainer: Trainer
+
+
+class Split(Enum):
+    TRAIN = "train"
+    VAL = "val"
+    TEST = "test"
+
+
+class Task(Enum):
+    REGRESSION = auto()
+    BINARY_CLASSIFICATION = auto()
+    MULTICLASS_CLASSIFICATION = auto()
+
+
+class RegressionOutput(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    preds: Tensor = Field(alias="logits")
+
+
+class BinaryClassificationOutput(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    logits: Tensor
+    probs: Tensor = Field(init=False)
+    preds: Tensor = Field(init=False)
+
+    @field_validator("probs", mode="before")
+    @classmethod
+    def compute_probs(cls, v, values):
+        return sigmoid(values["logits"])
+
+    @field_validator("preds", mode="before")
+    @classmethod
+    def compute_preds(cls, v, values):
+        return (values["probs"] > 0.5).float()
+
+
+class MulticlassClassificationOutput(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    logits: Tensor
+    probs: Tensor = Field(init=False)
+    preds: Tensor = Field(init=False)
+
+    @field_validator("probs", mode="before")
+    @classmethod
+    def compute_probs(cls, v, values):
+        return softmax(values["logits"], dim=1)
+
+    @field_validator("preds", mode="before")
+    @classmethod
+    def compute_preds(cls, v, values):
+        return values["probs"].argmax(dim=1)
