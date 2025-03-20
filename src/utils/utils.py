21e03663d78db68d5999b0caf66337ebff8e6b01
DrPeterPun <61242967+DrPeterPun@users.noreply.github.com>
2025-03-20 21:30:42 +0000

    Fix flatten version (#28)

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,4 @@
 import re
-from collections.abc import MutableMapping
 from functools import partial
 from typing import Any
 
@@ -7,7 +6,7 @@ from lightning import Trainer
 from omegaconf import DictConfig, OmegaConf
 
 
-def flatten(dictionary: MutableMapping, parent_key: str = "") -> dict[str, Any]:
+def flatten(dictionary: dict, parent_key: str = "") -> dict[str, Any]:
     """Flatten a nested dictionary into a single-level dictionary.
 
     :param dictionary: The nested dictionary to be flattened
@@ -21,11 +20,27 @@ def flatten(dictionary: MutableMapping, parent_key: str = "") -> dict[str, Any]:
     items: list[tuple[str, Any]] = []
     for key, value in dictionary.items():
         new_key = f"{parent_key}.{key}" if parent_key else key
-        if isinstance(value, MutableMapping):
+        if isinstance(value, dict):
             items.extend(flatten(value, parent_key=new_key).items())
         else:
             items.append((new_key, value))
     return dict(items)
+
+
+def containers_to_dict(container: dict | list | Any) -> dict | Any:
+    """Converts lists to dicts  of the form index:value, in a nested
+    combination of dicts and lists.
+
+    :param container: The nested combination of lists and dicts, or just
+        a value
+    :type container: dict | list | Any
+    """
+    if isinstance(container, dict):
+        return {k: containers_to_dict(v) for (k, v) in container.items()}
+    elif isinstance(container, list):
+        return containers_to_dict(dict(enumerate(container)))
+    else:
+        return container
 
 
 def log_cfg(cfg: DictConfig, trainer: Trainer) -> None:
@@ -36,7 +51,8 @@ def log_cfg(cfg: DictConfig, trainer: Trainer) -> None:
     :param trainer: _description_
     :type trainer: Trainer
     """
-    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+    cfg_cont = OmegaConf.to_container(cfg, resolve=True)
+    cfg_dict = containers_to_dict(cfg_cont)
     cfg_flat = flatten(cfg_dict)
 
     invalid_chars = re.compile(r"[^a-zA-Z0-9_\-.]")
