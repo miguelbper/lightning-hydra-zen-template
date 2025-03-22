8aa741f1e588bd6329ed1e128aa92d174d84902c
Miguel Pereira <miguel.pereira@signature.ai>
2025-03-22 12:05:37 +0000

    Simplify code logic in train / test

--- src/test.py
+++ src/test.py
@@ -6,14 +6,12 @@ from hydra.utils import instantiate
 from lightning import LightningDataModule, LightningModule, Trainer
 from omegaconf import DictConfig
 
-from src.utils.types import Metrics, Objects
-from src.utils.utils import log_cfg
-
 rootutils.setup_root(__file__)
 log = logging.getLogger(__name__)
 
 
-def test(cfg: DictConfig) -> tuple[Metrics, Objects]:
+@hydra.main(version_base="1.3", config_path="../configs", config_name="config.yaml")
+def test(cfg: DictConfig) -> None:
     """Test a model from a configuration object (which should include a
     checkpoint).
 
@@ -25,35 +23,14 @@ def test(cfg: DictConfig) -> tuple[Metrics, Objects]:
             - Metrics: A dictionary of metrics from testing
             - Objects: The objects (cfg, model, datamodule, trainer) used in the testing process
     """
-    log.info(f"Instantiating model <{cfg.model._target_}>...")
+    log.info("Instantiating objects...")
     model: LightningModule = instantiate(cfg.model)
-    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>...")
     datamodule: LightningDataModule = instantiate(cfg.datamodule)
-    log.info(f"Instantiating trainer <{cfg.trainer._target_}>...")
     trainer: Trainer = instantiate(cfg.trainer)
-    objects = Objects(cfg=cfg, model=model, datamodule=datamodule, trainer=trainer)
-
-    log_cfg(cfg, trainer)
 
     log.info("Testing model...")
     trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
-    metrics = trainer.callback_metrics
-
-    return metrics, objects
-
-
-@hydra.main(version_base="1.3", config_path="../configs", config_name="config.yaml")
-def main(cfg: DictConfig) -> None:
-    """Main function to evaluate the model based on the provided configuration.
-
-    Args:
-        cfg (DictConfig): Configuration object representing the config files.
-
-    Returns:
-        None
-    """
-    test(cfg)
 
 
 if __name__ == "__main__":
-    main()
+    test()
