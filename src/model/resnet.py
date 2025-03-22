68cd37e7e3485e234f5b821677492508c48d4118
Miguel Pereira <miguel.pereira@signature.ai>
2025-03-22 17:32:31 +0000

    Suppress type error in resnet loading

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -4,9 +4,13 @@ from torch import nn
 
 class ResNet(nn.Module):
     def __init__(self, num_classes: int = 10) -> None:
-        super().__init__()
+        super().__init__()  # type: ignore
         self.num_classes: int = num_classes
-        self.model: nn.Module = torch.hub.load("pytorch/vision:v0.10.0", "resnet18", num_classes=num_classes)  # nosec B614
+        self.model: nn.Module = torch.hub.load(  # nosec B614  # type: ignore
+            "pytorch/vision:v0.10.0",
+            "resnet18",
+            num_classes=num_classes,
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
