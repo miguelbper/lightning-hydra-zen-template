6c46af521b17d245e06ffcda9e2b1971fa60aed2
Miguel Pereira <miguel.pereira@signature.ai>
2025-03-22 21:26:02 +0000

    Fix warning on loading pretrained resnet weights

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,12 +1,12 @@
 import torch
 from torch import nn
-from torchvision.models import resnet18
+from torchvision.models import ResNet18_Weights, resnet18
 
 
 class ResNet(nn.Module):
     def __init__(self, num_classes: int = 10) -> None:
         super().__init__()  # type: ignore
-        self.model: nn.Module = resnet18(pretrained=True)
+        self.model: nn.Module = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
         self.model.fc = nn.Linear(512, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
