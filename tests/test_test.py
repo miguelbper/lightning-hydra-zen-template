313ae4d8fb7de50190bc8ba57f64f5db23c37660
Miguel Pereira <miguel.pereira@signature.ai>
2025-03-29 19:51:05 +0000

    Add fixture on test for test

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,19 +1,36 @@
+import pytest
+import torch
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, open_dict
+from pytest import FixtureRequest
 
-from src.test import test as evaluate
+from src.test import test as _test
 from src.train import train
 
 
-def test_train_and_test(cfg: DictConfig):
+@pytest.fixture(params=["cpu", "mps", "cuda"])
+def accelerator(request: FixtureRequest) -> str:
+    device: str = request.param  # type: ignore
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("CUDA is not available")
+    if device == "mps" and not torch.backends.mps.is_available():
+        pytest.skip("MPS is not available")
+    return device
+
+
+def test_train_and_test(cfg: DictConfig, accelerator: str):
     HydraConfig().set_config(cfg)
 
     with open_dict(cfg):
+        cfg.datamodule.batch_size = 2
         cfg.trainer.max_epochs = 1
         cfg.trainer.limit_train_batches = 1
+        cfg.trainer.limit_val_batches = 1
+        cfg.trainer.limit_test_batches = 1
+        cfg.trainer.accelerator = accelerator
         cfg.evaluate = False
     train(cfg)
 
     with open_dict(cfg):
         cfg.ckpt_path = f"{cfg.paths.output_dir}/checkpoints/last.ckpt"
-    evaluate(cfg)
+    _test(cfg)
