25dfaf011b922c96402ac452bcdf1acc5d1ba910
Miguel Pereira <miguel.pereira@signature.ai>
2025-04-07 19:43:53 +0100

    Undo changes in test and train

--- src/test.py
+++ src/test.py
@@ -1,20 +1,17 @@
 import logging
-from pathlib import Path
 
-from hydra_zen import ZenStore, zen
+import hydra
+import rootutils
+from hydra.utils import instantiate
 from lightning import LightningDataModule, LightningModule, Trainer
+from omegaconf import DictConfig
 
-from src.configs.config import Config
-
+rootutils.setup_root(search_from=__file__, dotenv=False)
 log = logging.getLogger(__name__)
 
 
-def test(
-    datamodule: LightningDataModule,
-    model: LightningModule,
-    trainer: Trainer,
-    ckpt_path: Path,
-) -> None:
+@hydra.main(version_base="1.3", config_path="../configs", config_name="config.yaml")
+def test(cfg: DictConfig) -> None:
     """Test a model from a configuration object (which should include a
     checkpoint).
 
@@ -24,16 +21,18 @@ def test(
     Returns:
         None
     """
+    log.info(f"Instantiating model <{cfg.model._target_}>")
+    model: LightningModule = instantiate(cfg.model)
+
+    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
+    datamodule: LightningDataModule = instantiate(cfg.datamodule)
+
+    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = instantiate(cfg.trainer)
+
     log.info("Testing model")
-    trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-
-
-def main() -> None:
-    store = ZenStore(deferred_hydra_store=False)
-    store(Config, name="config")
-    task_function = zen(test)
-    task_function.hydra_main(config_path=None, config_name="config", version_base="1.3")
+    trainer.test(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
 
 
 if __name__ == "__main__":
-    main()
+    test()
