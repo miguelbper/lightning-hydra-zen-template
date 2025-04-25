5594e2e44fde665b16e6e4d7491b639474b0544e
Miguel Barbosa Pereira <miguel.b.per@gmail.com>
2025-04-26 00:25:41 +0100

    Use rich to print the config

--- src/utils/print_cfg.py
+++ src/utils/print_cfg.py
@@ -2,6 +2,9 @@ import copy
 import logging
 import os
 
+import rich
+import rich.syntax
+import rich.tree
 from omegaconf import DictConfig, OmegaConf, flag_override
 
 log = logging.getLogger(__name__)
@@ -9,6 +12,8 @@ log = logging.getLogger(__name__)
 
 def print_cfg(cfg: DictConfig) -> None:
     file_path = os.path.join(cfg.paths.output_dir, "resolved_config.log")
+    log.info(f"Saving config to {file_path}")
+
     cfg = OmegaConf.create(OmegaConf.to_container(cfg, resolve=True))
 
     remove_packages = ["hydra", "paths", "logger", "callbacks"]
@@ -18,9 +23,15 @@ def print_cfg(cfg: DictConfig) -> None:
             with flag_override(cfg, ["struct", "readonly"], [False, False]):
                 cfg.pop(package)
 
-    yaml_str = OmegaConf.to_yaml(cfg)
+    tree = rich.tree.Tree("CONFIG")
 
-    log.info(f"Composed config saved to {file_path}")
-    print(yaml_str)
-    with open(file_path, "w") as f:
-        f.write(yaml_str)
+    for key, value in cfg.items():
+        is_dict = isinstance(value, DictConfig)
+        convert_fn = OmegaConf.to_yaml if is_dict else str
+        branch_content = convert_fn(value)
+        branch = tree.add(key)
+        branch.add(rich.syntax.Syntax(branch_content, "yaml", theme="gruvbox-dark"))
+
+    rich.print(tree)
+    with open(file_path, "w") as file:
+        rich.print(tree, file=file)
