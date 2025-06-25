a7a4812700ba98c7490f1796134b1494d73f34bc
Miguel Pereira <miguel.b.per@gmail.com>
2025-06-25 10:34:00 +0100

    Redo train script for compatibility with sklearn

--- src/lightning_hydra_zen_template/funcs/train.py
+++ src/lightning_hydra_zen_template/funcs/train.py
@@ -3,6 +3,7 @@ import logging
 import lightning as L
 import torch
 from lightning import LightningDataModule, LightningModule, Trainer
+from lightning.pytorch.utilities.types import _EVALUATE_OUTPUT
 
 log = logging.getLogger(__name__)
 
@@ -12,10 +13,10 @@ def train(
     model: LightningModule,
     trainer: Trainer,
     ckpt_path: str | None = None,
-    evaluate: bool = True,
     matmul_precision: str | None = None,
     compile: bool = False,
-) -> float:
+    return_all_metrics: bool = False,
+) -> float | dict[str, float]:
     """Train, validate and test a PyTorch Lightning model.
 
     Args:
@@ -23,34 +24,39 @@ def train(
         model (LightningModule): The PyTorch Lightning model to train.
         trainer (Trainer): The PyTorch Lightning trainer instance.
         ckpt_path (str | None, optional): Path to a checkpoint to resume training from. Defaults to None.
-        evaluate (bool | None, optional): Whether to run validation and testing after training. Defaults to True.
         matmul_precision (str | None, optional): Precision for matrix multiplication. Defaults to None.
         compile (bool | None, optional): Whether to compile the model using torch.compile(). Defaults to True.
+        return_all_metrics (bool, optional): Whether to return all metrics or just the best one. Defaults to False.
 
     Returns:
         float: The best model score achieved during training, or None if no score was recorded.
     """
+    if not hasattr(trainer, "checkpoint_callback"):
+        raise ValueError("Trainer does not have a checkpoint callback")
+    if not compatible_inputs(data=data, model=model, trainer=trainer):
+        raise ValueError("Data, model and trainer must be compatible")
+
     if matmul_precision:
         log.info(f"Setting matmul precision to {matmul_precision}")
         torch.set_float32_matmul_precision(matmul_precision)
 
-    if compile:
+    if compile and isinstance(model, torch.nn.Module):
         log.info("Compiling model")
         model = torch.compile(model)
 
     log.info("Training model")
     trainer.fit(model=model, datamodule=data, ckpt_path=ckpt_path)
-    metric: torch.Tensor | None = trainer.checkpoint_callback.best_model_score
     ckpt_path: str = trainer.checkpoint_callback.best_model_path
+    metric: float = trainer.checkpoint_callback.best_model_score.item()
 
-    if evaluate and ckpt_path:
-        log.info("Validating model")
-        trainer.validate(model=model, datamodule=data, ckpt_path=ckpt_path)
+    log.info("Validating model")
+    val_out: _EVALUATE_OUTPUT = trainer.validate(model=model, datamodule=data, ckpt_path=ckpt_path)
 
-        log.info("Testing model")
-        trainer.test(model=model, datamodule=data, ckpt_path=ckpt_path)
+    log.info("Testing model")
+    test_out: _EVALUATE_OUTPUT = trainer.test(model=model, datamodule=data, ckpt_path=ckpt_path)
 
-    return metric.item() if metric is not None else None
+    metrics: dict[str, float] = {**val_out[0], **test_out[0]}
+    return metrics if return_all_metrics else metric
 
 
 def seed_fn(seed: int) -> None:
@@ -61,3 +67,14 @@ def seed_fn(seed: int) -> None:
     """
     log.info(f"Setting seed to {seed}")
     L.seed_everything(seed, workers=True, verbose=False)
+
+
+def compatible_inputs(data: LightningDataModule, model: LightningModule, trainer: Trainer) -> bool:
+    """Check if the data, model and trainer are compatible.
+
+    Args:
+        data (LightningDataModule): The data module containing training, validation and test data.
+        model (LightningModule): The PyTorch Lightning model to train.
+        trainer (Trainer): The PyTorch Lightning trainer instance.
+    """
+    return True
